a44986699923a00e5aed1743e7dc3b8a85894f34
sidney <516202795@qq.com>
2023-09-22 22:58:02 +0800

    refine windows build

--- src/util.rs
+++ src/util.rs
@@ -59,29 +59,20 @@ pub fn format_date_time(dt: std::time::SystemTime) -> String {
 }
 
 pub fn get_creation_date(file_path: &str) -> String {
-    #[cfg(target_os = "windows")]
-    {
-        if let Ok(metadata) = fs::metadata(file_path) {
-            if let Ok(created) = metadata.created() {
-                // 将Windows文件时间格式化为RFC3339格式
-                return created.format("%Y-%m-%dT%H:%M:%SZ").to_string();
-            }
-        }
+    let os = std::env::consts::OS;
+    if os != "linux" && os != "macos" {
+        return "".to_string();
     }
+    if let Ok(metadata) = fs::metadata(file_path) {
+        let created = metadata.ctime();
 
-    #[cfg(all(not(target_os = "windows"), target_family = "unix"))]
-    {
-        if let Ok(metadata) = fs::metadata(file_path) {
-            let created = metadata.ctime();
-
-            // 将Unix时间戳格式化为RFC3339格式
-            let formatted_date = {
-                let secs = created as i64;
-                let datetime = NaiveDateTime::from_timestamp_opt(secs, 0);
-                datetime.expect("invalid or out-of-range datetime")
-            };
-            return formatted_date.format("%Y-%m-%dT%H:%M:%SZ").to_string();
-        }
+        // 将Unix时间戳格式化为RFC3339格式
+        let formatted_date = {
+            let secs = created as i64;
+            let datetime = NaiveDateTime::from_timestamp_opt(secs, 0);
+            datetime.expect("invalid or out-of-range datetime")
+        };
+        return formatted_date.format("%Y-%m-%dT%H:%M:%SZ").to_string();
     }
 
     "".to_string()
